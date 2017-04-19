0f3eb3752829e197f4185e0bbecbfebdb691a81a
kyungmin <juji6255@gmail.com>
2017-04-19 07:58:52 +0000

    transfer stolen

--- raspi/iBeacon-Scanner--master/testblescan.py
+++ raspi/iBeacon-Scanner--master/testblescan.py
@@ -11,6 +11,7 @@ info = ""
 a_distance = 0.0
 b_distance = 0.0
 stop_check = 0.0
+transferred_info = ""
 
 HOST = '192.168.1.2'
 PORT = 7622
@@ -54,7 +55,7 @@ class BeaconCondition(threading.Thread):
                 global stop_check
                 l_check = ""
                 count = 0
-                transferred_info = ""
+                global transferred_info
                 while True:
                         if info == 1:
                                 time.sleep(3)
@@ -83,8 +84,12 @@ class BeaconFinder(threading.Thread):
                 global a_distance
                 global b_distance
                 global stop_check
+                global transferred_info
                 
                 location_data = -1
+                f1_distance = 0.0
+                f2_distance = 0.0
+                count = 0
                 
                 while True:
                         if info == 1: # activate the beacon
@@ -96,7 +101,7 @@ class BeaconFinder(threading.Thread):
                                 distance_1 = 100.0
                                 distance_2 = 100.0
                                 print "-----[beacon finder]-----"
-                                while num_1 <= 4 and num_2 <= 4:
+                                while num_1 < 2 or num_2 < 2:
                                         returnList = blescan.parse_events(sock, 2)
                                         for beacon in returnList:
                                                 bea = beacon.split(',')
@@ -128,20 +133,57 @@ class BeaconFinder(threading.Thread):
                                         b_distance = distance_2
                                         print "major = 234, distance = " + str(distance_2) + ", checked_num = " + str(num_2)
 
-                                if distance_1 < distance_2:
-                                        print "39 beacon is closer"
+                                if count == 0:
+                                        if distance_1 < distance_2:
+                                                f2_distance = 100.0
+                                        else:
+                                                f1_distance = 100.0
+                                        count = count + 1
+                                
+                                if f2_distance == 100.0:
+                                        print "39 beacon"
+                                        if f1_distance == 0.0:
+                                                print "39 beacon is connected"
+                                                f1_distance = distance_1
+                                                f2_distance = 100.0
+                                        else:
+                                                if abs(distance_1 - f1_distance) > 0.5:
+                                                        if beacon_1_lost > 1:
+                                                                if transferred_info != "stolen"
+                                                                        s_1.sendall('{"ctname":"parking_state", "con":"stolen"}')
+                                                                        transferred_info = "stolen"
+                                                        beacon_1_lost = beacon_1_lost + 1
+                                                else:
+                                                        beacon_1_lost = 0
+                                                
                                         if location_data != 39:
                                                 s_1.sendall('{"ctname":"parking_location", "con":"39"}')
                                                 location_data = 39
                                         
-                                elif distance_2 < distance_1:
-                                        print "234 beacon is closer"
+                                elif f1_distance == 100.0:
+                                        print "234 beacon"
+                                        if f2_distance == 0.0:
+                                                print "234 beacon is connected"
+                                                f1_distance = 100.0
+                                                f2_distance = distance_2
+                                        else:
+                                                if abs(distance_2 - f2_distance) > 0.5:
+                                                        if beacon_2_lost > 1:
+                                                                if transferred_info != "stolen"
+                                                                        s_1.sendall('{"ctname":"parking_state", "con":"stolen"}')
+                                                                        transferred_info = "stolen"
+                                                        beacon_2_lost = beacon_2_lost + 1
+                                                else:
+                                                        beacon_2_lost = 0
                                         if location_data != 234:
                                                 s_1.sendall('{"ctname":"parking_location", "con":"234"}')
                                                 location_data = 234
 
                         else: # deactivate the beacon
-                                time.sleep(1)
+                                time.sleep(2)
+                                f1_distance = 0.0
+                                f2_distance = 0.0
+                                count = 0
 
 
                                 
