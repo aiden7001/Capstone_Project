4d4e52f136c9138ade4805edc38942eddcfb9189
kyungmin <juji6255@gmail.com>
2017-06-16 11:53:37 +0000

    5/16 ; 21:00

--- raspi/test.py
+++ raspi/test.py
@@ -10,33 +10,81 @@ gpio.setup(20, gpio.OUT)
 gpio.output(20,False)
 gpio.setup(21, gpio.OUT)
 gpio.output(21,False)
-
-class light_20(threading.Thread):
+gpio.setup(6, gpio.OUT) # left_trig
+gpio.setup(5, gpio.IN) # left_echo
+gpio.setup(26, gpio.OUT) # buzzor
+gpio.setup(13, gpio.OUT) # right_trig
+gpio.setup(19, gpio.IN) # right_echo
+        
+class left_light(threading.Thread):
     def run(self):
+        global left_check
         while True:
             if gpio.input(23) == 0:
+                gpio.output(6, False)
                 time.sleep(0.5)
+                gpio.output(6, True)
+                time.sleep(0.00001)
+                
+                gpio.output(6, False)
+                
+                while gpio.input(5) == 0 :
+                    pulse_start = time.time()
+                while gpio.input(5) == 1 :
+                    pulse_end = time.time()
+                    pulse_duration = pulse_end - pulse_start
+                    distance = pulse_duration * 17000
+                    distance = round(distance, 2)
+                    
+                if distance < 20:
+                    print "left_Distance : ", distance, "cm"
+                    gpio.output(26, True)
+                    
                 gpio.output(20,True)
                 time.sleep(0.5)
                 gpio.output(20,False)
+                gpio.output(26,False)
             else:
                 time.sleep(0.3)
 
-class light_21(threading.Thread):
+                
+
+class right_light(threading.Thread):
     def run(self):
         while True:
             if gpio.input(24) == 0:
+                gpio.output(13, False)
                 time.sleep(0.5)
+                gpio.output(13, True)
+                time.sleep(0.00001)
+
+                gpio.output(13, False)
+                
+                while gpio.input(19) == 0 :
+                    pulse_start = time.time()
+                    
+                while gpio.input(19) == 1 :
+                    pulse_end = time.time()
+                    pulse_duration = pulse_end - pulse_start
+                    distance = pulse_duration * 17000
+                    distance = round(distance, 2)
+                    
+                if distance < 20:
+                    print "right_Distance : ", distance, "cm"
+                    gpio.output(26, True)
+                
                 gpio.output(21,True)
                 time.sleep(0.5)
                 gpio.output(21,False)
+                gpio.output(26,False)
+                
             else:
                 time.sleep(0.3)
             
 try:
-    light_0 = light_20()
+    light_0 = left_light()
     light_0.start()
-    light_1 = light_21()
+    light_1 = right_light()
     light_1.start()
     a = input()
         
